64be8b0e846b6a397ff4ec154e78f57aca8ce51d
Sohil Patel <46017238+sohilp393@users.noreply.github.com>
2020-03-26 21:07:28 +0530

    Delete event_org.rb

--- app/models/event_org.rb
+++
@@ -1,7 +0,0 @@
-class EventOrg < ApplicationRecord
-	has_many :eventstates
-  	has_many :users, through: :eventstates
-
-	validates :eventname, presence:true ,length:{mininmum:3 ,maximum:50}
-	validates :description, presence:true ,length:{mininmum:10 ,maximum:500}
-end
