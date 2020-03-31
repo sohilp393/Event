118271155304c0bd40dbc141898958f907745c4a
sohilp393 <Sohilp393@gmail.com>
2020-03-31 22:23:03 +0530

    Sixth commit - added states controller

--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -2,16 +2,24 @@ class StatesController < ApplicationController
 
   def update 
     @state = State.find_by(evennt_id:params[:evennt_id],user_id:params[:user_id])
+    debugger
     if @state.update(eventstate: params[:state])
       redirect_to  users_path
     end
   end
 
-
-private
-
-  def email_params 
-    params.require(:user).permit(:email)
+  def adduser 
+    @user=User.find_by(email: params[:user][:email])
+    @state = State.new
+    @state.user_id = @user.id
+    @state.evennt_id = params[:evennt_id]
+    debugger 
+    if @state.save
+       redirect_to  users_path 
+    else
+      redirect_to  users_path
+    end
   end
+
 
 end
