b25b1bef502b44844ab06a4175680dfd95d86aec
sohilp393 <Sohilp393@gmail.com>
2020-04-02 21:20:42 +0530

    Removed unnecessay files

--- db/migrate/20200401094941_add_createdby_to_evennts.rb
+++
@@ -1,5 +0,0 @@
-class AddCreatedbyToEvennts < ActiveRecord::Migration[6.0]
-  def change
-    add_column :evennts, :createdby, :integer
-  end
-end
--- db/migrate/20200401120704_drop_user.rb
+++
@@ -1,9 +0,0 @@
-class DropUser < ActiveRecord::Migration[6.0]
-  def up
-    drop_table :users
-  end
-
-  def down
-    raise ActiveRecord::IrreversibleMigration
-  end
-end
