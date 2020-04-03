e071352555d9d9648fee30003547c22d7d9e87b0
sohilp393 <Sohilp393@gmail.com>
2020-04-03 22:29:25 +0530

    remove the unwanted code from user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,12 +22,6 @@ class UsersController < ApplicationController
     end
  	end
 
-	def destroy 
- 		@user = User.find(params[:id])  
-    @user.destroy
-    redirect_to users_path
-   end
-
 private
 	def user_params
 		params.require(:user).permit(:username, :email, :password)
