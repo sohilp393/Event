6a05c958bdb1fbf300e963c2c3a0a595aeb3f6e7
sohilp393 <Sohilp393@gmail.com>
2020-04-03 22:31:55 +0530

    ..

--- app/models/evennt.rb
+++ app/models/evennt.rb
@@ -4,4 +4,7 @@ class Evennt < ApplicationRecord
 
 	validates :eventname, presence:true ,length:{mininmum:3 ,maximum:50}
 	validates :description, presence:true ,length:{mininmum:10 ,maximum:500}
+
+  attr_accessor :email
+
 end
