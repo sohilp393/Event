f71fc4962c73461cea8841cfff26daa16a31f8a8
sohilp393 <Sohilp393@gmail.com>
2020-04-06 21:13:24 +0530

    Added the code to display the record on dashboard and also to change the state

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,14 +1,26 @@
 class DashboardController < ApplicationController
 
   def allevents
-    @user = current_user 
-    @evennts = Evennt.where(createdby:@user.id)
+    @allevents_hash = Hash.new
+    @user = current_user
+
+    @created_evennts = Evennt.where(createdby:@user.id)
+    @invitedin_events = State.where(user_id:@user.id,eventstate:["Accepted","Rejected"])
+   
+    @allevents_hash = {"Createdby"=> @created_evennts,"InvitedIn" => @invitedin_events} 
   end
 
 
   def pendinginvite
     @user = current_user
     @states = State.where(user_id:current_user.id,eventstate:"Pending")
-    @evennts = Evennt.all
   end
+
+  def updatestate 
+    @state = State.find_by(evennt_id:params[:evennt_id],user_id:params[:user_id])
+    if @state.update(eventstate: params[:state])
+      redirect_to  dashboard_path
+    end
+  end
+  
 end
