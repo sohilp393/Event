7d7344f385d1f9122618e51dac3ca8c49d53a5ee
sohilp393 <Sohilp393@gmail.com>
2020-04-06 21:16:10 +0530

    Fix the issue when adding sinle and multiple user

--- app/controllers/evennts_controller.rb
+++ app/controllers/evennts_controller.rb
@@ -5,7 +5,12 @@ class EvenntsController < ApplicationController
   end
 
   def show 
-    @evennt = Evennt.find(params[:id]) 
+    @evennt = Evennt.find_by(id:params[:id],createdby:current_user.id) 
+    if @evennt.nil?
+      redirect_to evennts_path
+    else
+      @evennt
+    end
   end
 
   def new
@@ -17,37 +22,29 @@ class EvenntsController < ApplicationController
   end
 
   def create
-    @evennt = Evennt.new(evennt_params.except(:email))
-    
-    if evennt_params[:email].include?(",")
-      evennt_params[:email].split(",").each do |e|
-        @evennt.users << User.find_by(email: e)
-      end
-    end
-
+    @evennt = Evennt.new(evennt_params.except(:email))    
+    evennt_params[:email].split(",").each do |e|
+      @evennt.users << User.find_by(email: e)
+    end 
     @evennt.createdby = current_user.id
-    if @evennt.save
-      redirect_to evennt_path(@evennt)
-    else
-      render 'new'
-    end
+      if @evennt.save
+        redirect_to evennt_path(@evennt)
+      else
+        render 'new'
+      end
   end
 
   def update
     @evennt = Evennt.find(params[:id])
-    
-      if evennt_params[:email].include?(",")
-        evennt_params[:email].split(",").each do |e|
-          @evennt.users << User.find_by(email: e)
-        end
-       end
+    evennt_params[:email].split(",").each do |e|
+      @evennt.users << User.find_by(email: e)
+    end
 
-      debugger
-      if @evennt.update(evennt_params)
-        redirect_to evennt_path(@evennt)
-      else
-        render 'edit'       
-      end
+    if @evennt.update(evennt_params)
+      redirect_to evennt_path(@evennt)
+    else
+      render 'edit'       
+    end
   end
 
   def destroy 
